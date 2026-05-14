12368499cf0ade6750c22cb38bb31f7d83b959af
mailtest6767-glitch <mailtest6767@gmail.com>
2026-05-14 14:20:22 +0300

    Create TestAddon.lua

---
+++ src/TestAddon.lua
@@ -0,0 +1,17 @@
+--[[
+многострочный
+комментарий
+]]
+local param1 = 1
+--[[
+другой
+комментарий
+]]--
+local param2 = 2
+
+local param3 = 3 -- 3
+
+local param4 = 4
+if param1 then
+  param1 = 5
+end
